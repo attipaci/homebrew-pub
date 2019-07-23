8a0d5076718ec058ddbffb34628f07a0ef00c866
Attila Kovacs <attipaci@gmail.com>
2019-07-23 12:09:54 -0400

    Many fixes. Passes audit...

--- crush.rb
+++ crush.rb
@@ -1,49 +1,42 @@
 class Crush < Formula
-  desc "Data reduction and imaging for select astronomical cameras"
+  desc "Data reduction and imaging pipeline for select astronomical cameras"
   homepage "https://www.sigmyne.com/crush"
   url "https://github.com/attipaci/crush/releases/download/2.50-1/crush-2.50-1.tar.gz"
   sha256 "e5c95d0dd2fa99a865ae04d6fe8260070cd8507f1cdcce6ad14aef35fba7963f"
 
-  depends_on "bash"					# >= 3.0
-  depends_on "coreutils"			# >= 5.0
-  #depends_on ":java" => "1.8"		# Any formula providing a java >= 1.8.0
-  depends_on "jdk"					# An alternative to :java above...
+  bottle :unneeded
+
+  depends_on ":java" => "1.8+"
   depends_on "gnuplot" => :recommended
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Install the various bits and pieces to their proper locations
+    share.install Dir["Install/posix/share/*"]
+    man.install Dir["Install/posix/man/*"]
+    doc.install Dir["Documentation/*"]
+    doc.install ["copyright", "license.txt"]
 
-	# Install the various bits and pieces to their proper locations
-	share.install Dir["Install/posix/share/*"]
-	man.install Dir["Install/posix/man/*"]
-	doc.install Dir["Documentation/*"]
-	doc.install "copyright"
-	doc.install "license.txt"
+    # Delete files installed elsewhere from #{pkgshare}
+    rm_rf "Install"
+    rm_rf "Documentation"
+    rm_f "copyright"
+    rm_f "license"
 
-	# Delete the installer files from #{prefix} (they are no longer needed)
-	system "rm", "-rf", "#{pkgshare}/Install"
+    # Copy the rest of the distro into its final location in #{prefix}/share/crush
+    pkgshare.install Dir["*"]
 
-	# Delete the empty Documentation directory...
-	system "rm", "-rf", "#{pkgshare}/Documentation"
-
-	# Copy the rest of the distro into its final location in #{prefix}/share/crush
-	pkgshare.install Dir["./*"]
-
-	# Create symlinks to the executables in #{bin} 
-	bin.install_symlink "#{pkgshare}/crush"
-	bin.install_symlink "#{pkgshare}/coadd"
-	bin.install_symlink "#{pkgshare}/detect"
-	bin.install_symlink "#{pkgshare}/esorename"
-	bin.install_symlink "#{pkgshare}/histogram"
-	bin.install_symlink "#{pkgshare}/image-diff"
-	bin.install_symlink "#{pkgshare}/imagetool"
-	bin.install_symlink "#{pkgshare}/show"
+    # Create symlinks to the executables in #{bin}
+    bin.install_symlink "#{pkgshare}/crush"
+    bin.install_symlink "#{pkgshare}/coadd"
+    bin.install_symlink "#{pkgshare}/detect"
+    bin.install_symlink "#{pkgshare}/esorename"
+    bin.install_symlink "#{pkgshare}/histogram"
+    bin.install_symlink "#{pkgshare}/difftool"
+    bin.install_symlink "#{pkgshare}/imagetool"
+    bin.install_symlink "#{pkgshare}/show"
   end
 
   test do
-    assert_predicate "#{bin}/crush", :exist?
-	assert_predicate "#{bin}/imagetool", :exist?
-	assert_predicate "#{bin}/show", :exist?
     system "#{bin}/crush"
   end
 end
