1d79070441d138f8db1c66029b455eff7b03e2c2
Attila Kovacs <attipaci@gmail.com>
2025-10-23 06:00:29 -0400

    supernovas v1.5.0-rc8

---
+++ supernovas.rb
@@ -0,0 +1,43 @@
+class Supernovas < Formula
+  desc "High-precision C/C++ astrometry library"
+  homepage "https://smithsonian.github.io/SuperNOVAS/"
+  url "https://github.com/Smithsonian/SuperNOVAS/archive/refs/tags/v1.5.0-rc8.tar.gz"
+  sha256 "51c555d1e7a940805b5ccc0321b22f3a4da398b8e33889fd70d6ab6e23748e02"
+  license "Unlicense"
+  head "https://github.com/Smithsonian/SuperNOVAS.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "calceph"
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DENABLE_CALCEPH=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"testsupernovas.c").write <<~C
+      #include <novas.h>
+      #include <assert.h>
+
+      int main () {
+        double dpsi, deps;
+        int ret = nutation_angles(NOVAS_JD_J2000, NOVAS_FULL_ACCURACY, &dpsi, &deps);
+        assert (ret == 0);
+        return 0;
+      }
+    C
+    system ENV.cc, "testsupernovas.c", "-L#{lib}", "-lsupernovas", "-o", "testsupernovas"
+    system "./testsupernovas"
+  end
+end
